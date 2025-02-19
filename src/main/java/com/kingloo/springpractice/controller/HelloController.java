2a4b23ea0c97a65a92fa2b8179830ec18baf760f
KimGiii <gibo9809@gmail.com>
2025-02-19 21:37:35 +0900

    회원 도메인, 저장소 생성 / 관련 테스트 코드 추가

--- src/main/java/com/kingloo/springpractice/controller/HelloController.java
+++ src/main/java/com/kingloo/springpractice/controller/HelloController.java
@@ -22,14 +22,15 @@ public class HelloController {
     }
 
     @GetMapping("hello-string")
-    @ResponseBody
+    @ResponseBody  // html의 body 부분에 내가 직접 데이터를 넣어주겠다라는 뜻인거임
     public String helloString(@RequestParam("name") String name) {
         return "hello " + name;  // name이 요청한 것 그대로
     }
 
     // json 방식
-    // Controller에서 받은 요청을 처리하기 위해 HttpMessageConverter가 동작한다
-    // jackson, Gson 두가지 라이브러리가 있음(객체를 json으로 바꾸는것)
+    // Controller 에서 받은 요청을 처리하기 위해 HttpMessageConverter 가 동작한다
+    // jackson, Gson 두가지 라이브러리가 있음(객체를 json 으로 바꾸는것)
+    // API는 객체를 반환하는 방식, view 없이 바로 http response 에 값을 넣어줌 그냥
     @GetMapping("hello-api")
     @ResponseBody
     public Hello helloApi(@RequestParam("name") String name) {
---
+++ src/test/java/com/kingloo/springpractice/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.kingloo.springpractice.repository;
+
+import com.kingloo.springpractice.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+// 여기서 Test 코드를 돌려서 클래스 단위로 테스트도 가능
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    // 테스트는 서로 의존관계가 없이 설계되어야함
+    // 그래서 각 메소드의 테스트가 끝날때마다 데이터가 비워지도록 해주는 코드임
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        // Assertions 에 option + Enter을 쳐서 static으로하면 바로 assertThat 사용 가능
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result).hasSize(2);
+    }
+}
