55fbb05ef52e3c08030403628369e1be17bc44e3
KimGiii <gibo9809@gmail.com>
2025-02-26 21:13:03 +0900

    JDBC 연결

--- src/main/java/com/kingloo/springpractice/SpringConfig.java
+++ src/main/java/com/kingloo/springpractice/SpringConfig.java
@@ -1,14 +1,25 @@
 package com.kingloo.springpractice;
 
+import com.kingloo.springpractice.repository.JdbcMemberRepository;
 import com.kingloo.springpractice.repository.MemberRepository;
 import com.kingloo.springpractice.repository.MemoryMemberRepository;
 import com.kingloo.springpractice.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // java 코드를 사용해 직접 Bean 등록
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
